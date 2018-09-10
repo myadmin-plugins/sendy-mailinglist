1bdb85928c1fba23ad0b97c79f05ea984120a068
Joe Huss <detain@interserver.net>
2018-09-10 02:54:17 -0400

    formatting updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,8 +9,8 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  *
  * @package Detain\MyAdminSendy
  */
-class Plugin {
-
+class Plugin
+{
 	public static $name = 'Sendy Plugin';
 	public static $description = 'Allows handling of Sendy based Mailing List Subscriptions';
 	public static $help = '';
@@ -19,13 +19,15 @@ class Plugin {
 	/**
 	 * Plugin constructor.
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 	}
 
 	/**
 	 * @return array
 	 */
-	public static function getHooks() {
+	public static function getHooks()
+	{
 		return [
 			'system.settings' => [__CLASS__, 'getSettings'],
 			'account.activated' => [__CLASS__, 'doAccountActivated'],
@@ -37,25 +39,30 @@ class Plugin {
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function doAccountActivated(GenericEvent $event) {
+	public static function doAccountActivated(GenericEvent $event)
+	{
 		$account = $event->getSubject();
-		if (defined('SENDY_ENABLE') && SENDY_ENABLE == 1)
+		if (defined('SENDY_ENABLE') && SENDY_ENABLE == 1) {
 			self::doSetup($account->getId());
+		}
 	}
 
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function doMailinglistSubscribe(GenericEvent $event) {
+	public static function doMailinglistSubscribe(GenericEvent $event)
+	{
 		$email = $event->getSubject();
-		if (defined('SENDY_ENABLE') && SENDY_ENABLE == 1)
+		if (defined('SENDY_ENABLE') && SENDY_ENABLE == 1) {
 			self::doEmailSetup($email);
+		}
 	}
 
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function getSettings(GenericEvent $event) {
+	public static function getSettings(GenericEvent $event)
+	{
 		$settings = $event->getSubject();
 		$settings->add_dropdown_setting('Accounts', 'Sendy', 'sendy_enable', 'Enable Sendy', 'Enable/Disable Sendy Mailing on Account Signup', (defined('SENDY_ENABLE') ? SENDY_ENABLE : '0'), ['0', '1'], ['No', 'Yes']);
 		$settings->add_text_setting('Accounts', 'Sendy', 'sendy_api_key', 'API Key', 'API Key', (defined('SENDY_API_KEY') ? SENDY_API_KEY : ''));
@@ -66,24 +73,27 @@ class Plugin {
 	/**
 	 * @param $accountId
 	 */
-	public static function doSetup($accountId) {
+	public static function doSetup($accountId)
+	{
 		$data = $GLOBALS['tf']->accounts->read($accountId);
-		self::doEmailSetup($data['account_lid'], isset($data['name']) ? ['name' => $data['name']] : FALSE);
+		self::doEmailSetup($data['account_lid'], isset($data['name']) ? ['name' => $data['name']] : false);
 	}
 
 	/**
 	 * @param                  $email
 	 * @param array|bool|false $params
 	 */
-	public static function doEmailSetup($email, $params = FALSE) {
+	public static function doEmailSetup($email, $params = false)
+	{
 		myadmin_log('accounts', 'info', "sendy_setup($email) Called", __LINE__, __FILE__);
 		$postarray = [
 			'email' => $email,
 			'list' => SENDY_LIST_ID,
 			'boolean' => 'true'
 		];
-		if ($params !== FALSE)
+		if ($params !== false) {
 			$postarray = array_merge($postarray, $params);
+		}
 		$postdata = http_build_query($postarray);
 		$opts = [
 			'http' => [
@@ -93,8 +103,9 @@ class Plugin {
 			]
 		];
 		$context = stream_context_create($opts);
-		$result = trim(file_get_contents(SENDY_APIURL.'/subscribe', FALSE, $context));
-		if ($result != '1')
+		$result = trim(file_get_contents(SENDY_APIURL.'/subscribe', false, $context));
+		if ($result != '1') {
 			myadmin_log('accounts', 'info', "Sendy Response: {$result}", __LINE__, __FILE__);
+		}
 	}
 }
